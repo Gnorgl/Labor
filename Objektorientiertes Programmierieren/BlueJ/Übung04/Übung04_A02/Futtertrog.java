6363fa9ba4a59c4b07afcad567979652cf72943e
Gnorgel <rijschroeder@gmail.com>
2025-12-08 20:02:40 +0100

    Ü4_B

--- Objektorientiertes Programmierieren/BlueJ/Übung04/Übung04_A02/Futtertrog.java
+++ Objektorientiertes Programmierieren/BlueJ/Übung04/Übung04_A02/Futtertrog.java
@@ -16,21 +16,98 @@ public class Futtertrog {
     }
     
     public void zeichnen() {
+        //Erste Hälfte Malen:
+        
+        int haelfte = (this.groesse - 1)/2;
+        int count = 1;
+        int space = 1;
+        
+        for(int i = haelfte; i > 0; i--) {
+            if(i == haelfte) {
+                for(int j = groesse; j > 0; j--) {
+                    if(j == haelfte + 1) {
+                        System.out.print(".");
+                    } else {
+                        System.out.print(" ");
+                    }
+                }
+            } else {
+                for(int j = haelfte - space; j > 0; j--) {
+                    System.out.print(" ");
+                    if(j == 1) {
+                        System.out.print("/");
+                        for(int k = count; k > 0; k--) {
+                            System.out.print(" ");
+                        }
+                        System.out.print("\\");
+                    }
+                }
+                space += 1;
+                count += 2;
+            }
+
+            System.out.println("");
+
+        }
+        
+        //Mitte Malen:
+        
+        System.out.print(".");
+        for(int i = groesse - 2; i > 0; i--) {
+            System.out.print(" ");
+        }
+        System.out.print(".");
+        
+        System.out.println("");
+        
+        //Zweite Hälfte Malen:
+        
+        for(int i = haelfte; i > 0; i--) {
+            if(i == 1) {
+                for(int j = groesse; j > 0; j--) {
+                    if(j == haelfte + 1) {
+                        System.out.print(".");
+                    } else {
+                        System.out.print(" ");
+                    }
+                }
+            } else {
+                space -= 1;
+                count -= 2;
+                for(int j = haelfte - space; j > 0; j--) {
+                    System.out.print(" ");
+                    if(j == 1) {
+                        System.out.print("\\");
+                        for(int k = count; k > 0; k--) {
+                            System.out.print(" ");
+                        }
+                        System.out.print("/");
+                    }
+                }
+            }
+
+            System.out.println("");
+
+        }
         
     }
     
     public static void main(String args[]) {
         Scanner scanner = new Scanner(System.in);
         int input;
-        
+        String nochmal;
+        //do-schleife:
         do {
-            System.out.println("Gib eine ganze Zahl ein");
-            input = Integer.valueOf(scanner.nextLine());
-        } while (input < 5 || input > 50 || input % 2 == 0);
-        
-        Futtertrog futtertrog = new Futtertrog(input);
-        futtertrog.zeichnen();
-        
+            do {
+                System.out.println("Gib eine ganze Zahl ein");
+                input = Integer.valueOf(scanner.nextLine());
+            } while (input < 5 || input > 50 || input % 2 == 0);
+            //wenn nummer größer/gleich 5 oder nummer kleiner/gleich 50 und ungerade ist:
+            Futtertrog futtertrog = new Futtertrog(input);
+            futtertrog.zeichnen();
+            System.out.println("Nochmal? (y/n)");
+            nochmal = scanner.nextLine();
+        } while(nochmal.equals("y"));
     }
     
     
