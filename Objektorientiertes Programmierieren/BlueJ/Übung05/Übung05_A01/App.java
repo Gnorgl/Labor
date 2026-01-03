64ed07eb7e39839f03b77d3ea312d15d72b1e91d
Gnorgel <rijschroeder@gmail.com>
2026-01-03 03:04:24 +0100

    MergeSort Hinweis

--- Objektorientiertes Programmierieren/BlueJ/Übung05/Übung05_A01/App.java
+++ Objektorientiertes Programmierieren/BlueJ/Übung05/Übung05_A01/App.java
@@ -1,27 +1,42 @@
 public class App {
     public static void main(String args[]) {
-        
     }
     
     public static int[] mergeArrays(int[] a, int[] b) {
         int[] c = new int[a.length + b.length];
         int[] d = new int[c.length];
+
+        return d;
         
-        for(int i = 0; i < a.length; i++) {
-            c[i] = a[i];
-        }
+        /** Funktioniert nicht:
+         * Was wenn arrays unterschiedlich lang, was wenn Werte doppelt?
+         * 
+         * Prinzip von MergeSort benutzen. Kartenstapel!
+         */
+        /*
+        //Wie lang soll for-loop sein:
         
-        for(int i = a.length - 1; i < c.length; i++) {
-            c[i] = b[i];
+        if(a.length > b.length) {
+            length = a.length;
+        } else if(a.length < b.length) {
+            length = b.length;
+        } else if (a.length == b.length) {
+            length = a.length;
         }
         
         //Vergleich, kleinsten Wert finden.
         
-        for(int i = 0; i < c.length; i++) {
-            d[i] = c[i];
+        for(int i = 0; i < length; i++) {
+            if(a[i] < b[i]) {
+                c[i] = a[i];
+                c[i+1] = b[i];
+            } else if(a[i] > b[i]) {
+                c[i] = b[i];
+                c[i+1] = a[i];
+            } else if (a[i] == b[i]) {
+                c[i] = a[i];
+            }
         }
-        
-        
-        return c;
+        */
     }
 }
