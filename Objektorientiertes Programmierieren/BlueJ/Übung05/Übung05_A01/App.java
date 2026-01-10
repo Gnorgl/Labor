11222e90bf03953ca1dac0ef777b36c2e4d4e513
Gnorgel <rijschroeder@gmail.com>
2026-01-10 23:40:44 +0100

    Übung05_A01

--- Objektorientiertes Programmierieren/BlueJ/Übung05/Übung05_A01/App.java
+++ Objektorientiertes Programmierieren/BlueJ/Übung05/Übung05_A01/App.java
@@ -1,66 +1,53 @@
 public class App {
     public static void main(String args[]) {
+        int[] a = {5, 6, 7, 10, 20};
+        int[] b = {3, 4, 5, 9};
+        
+        System.out.println(java.util.Arrays.toString(mergeArrays(a, b)));
     }
     
     public static int[] mergeArrays(int[] a, int[] b) {
-        int[] c = new int[a.length + b.length];
-        int[] d = new int[c.length];
 
-        return d;
+        int[] neuesArray = new int[a.length + b.length]; 
         
-        /** Funktioniert nicht:
+        int i = 0; //Für a
+        int j = 0; //Für b
+        
+        int k = 0; //Für neuesArray der Counter
+        
+        while(i < a.length && j < b.length) {
+            if(a[i] <= b[j]) {
+                neuesArray[k] = a[i];
+                k++;
+                i++;
+            } else {
+                neuesArray[k] = b[j];
+                k++;
+                j++;
+            }
+        }
+        
+        //Was wenn ein Array länger ist als das andere?
+        //Müssen kontrollieren:
+        
+        while(i < a.length) {
+            neuesArray[k] = a[i];
+            k++;
+            i++;
+        }
+        
+        while(j < b.length) {
+            neuesArray[k] = b[j];
+            k++;
+            j++;
+        }
+        
+        return neuesArray;
+
+        /**
          * Was wenn arrays unterschiedlich lang, was wenn Werte doppelt?
          * 
          * Prinzip von MergeSort benutzen. Kartenstapel!
          */
-        /*
-        //Wie lang soll for-loop sein:
-        
-        if(a.length > b.length) {
-            length = a.length;
-        } else if(a.length < b.length) {
-            length = b.length;
-        } else if (a.length == b.length) {
-            length = a.length;
-        }
-        
-        //Vergleich, kleinsten Wert finden.
-        
-        for(int i = 0; i < length; i++) {
-            if(a[i] < b[i]) {
-                c[i] = a[i];
-                c[i+1] = b[i];
-            } else if(a[i] > b[i]) {
-                c[i] = b[i];
-                c[i+1] = a[i];
-            } else if (a[i] == b[i]) {
-                c[i] = a[i];
-            }
-        }
-        
-        // Beispielhafte Merge-Funktion
-        public static void merge(int[] arr, int[] links, int[] rechts) {
-            int i = 0, j = 0, k = 0;
-        
-            // Solange in beiden Arrays noch Elemente sind
-            while (i < links.length && j < rechts.length) {
-                if (links[i] <= rechts[j]) {
-                    arr[k++] = links[i++];
-                } else {
-                    arr[k++] = rechts[j++];
-                }
-            }
-        
-            // Falls links noch Reste sind (z.B. die 11 und 12)
-            while (i < links.length) {
-                arr[k++] = links[i++];
-            }
-        
-            // Falls rechts noch Reste sind
-            while (j < rechts.length) {
-                arr[k++] = rechts[j++];
-            }
-        }
-        */
     }
 }
