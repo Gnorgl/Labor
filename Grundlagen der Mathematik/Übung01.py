8626c5b92ddc5c2b871f59090572de53055aec52
Gnorgel <rijschroeder@gmail.com>
2026-03-05 20:25:07 +0100

    Update Übung01.py

--- Grundlagen der Mathematik/Übung01.py
+++ Grundlagen der Mathematik/Übung01.py
@@ -1,7 +1,51 @@
-import matplotlib.pyplot as plt
 import numpy as np
-from scipy.integrate import quad
-from numpy import*
-import pandas as pd
+import matplotlib.pyplot as plt #matplotlib ist sehr große bibliothek, man will nur pyplot haben.
+import scipy as sp
 
-#Test
+#Aufgabe 1:
+
+#a)
+n = 10 #Wird erst nachgefragt
+a = 1 #Wird erst nachgefragt
+summe = 0
+
+for k in range(a, n + 1):
+    quadrat = pow(k, 2)
+    summe += quadrat
+
+print(summe)
+
+#b)
+n = 10
+produkt = 1
+
+for k in range(1, n + 1):
+    produkt = produkt*2*k
+
+print(produkt)
+
+#c)
+n = 10
+summe = 0
+
+for k in range(1, n + 1):
+    summe += k
+
+print(summe / (n + 1))
+
+#d)
+print(pow(n, 2))
+
+#oder:
+
+import math
+
+n = 10
+# Die direkte (unvereinfachte) Umsetzung:
+ergebnis_kompliziert = math.exp(2 * math.log(n))
+
+# Deine Lösung:
+ergebnis_smart = n**2 # oder pow(n, 2)
+
+#Aufgabe 2:
+
